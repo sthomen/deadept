530767a24d87ffbde6ad3bd4d60a161a5b9d62fd
Staffan Thomen <duck@shangtai.net>
2019-07-05 14:33:27 +0300

    Remove the exception wrapping around loading the file in keyfile, it is pointless

--- deadept/keyfile/keyfile.py
+++ deadept/keyfile/keyfile.py
@@ -4,10 +4,7 @@ from ..platform import Platform, PlatformException
 
 class Keyfile(Platform):
 	def getKey(self):
-		try:
-			with open('adept.der', 'br') as fp:
-				data = fp.read()
-		except (IOError, FileNotFoundError):
-			raise PlatformException("Couldn't load user key from 'adept.der' in the current directory")
+		with open('adept.der', 'br') as fp:
+			data = fp.read()
 
 		return RSA.import_key(data)
